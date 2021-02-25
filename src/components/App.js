c2cedeaa4876454f7d92fb29daf8d625c3e57e77
Marc <marc.ferraro1@gmail.com>
2021-02-25 13:22:04 -0600

    Filter with info bug

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,40 @@ import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
 import HogContainer from "./Hogcontainer";
+import Filter from './Filter'
 
 class App extends Component {
 
   constructor(){
     super()
     this.state = {
-      hogs: hogs
+      hogs: hogs,
+      greased: false
+    }
+  }
+
+  toggleGreased = () => {
+    console.log('greasy')
+    // this.setState({
+    //   greased: !this.state.greased
+    // })
+    let greasedHogs = []
+
+    if (!this.state.greased){
+      greasedHogs = this.state.hogs.filter(hog => {
+        // console.log(hog)
+        return hog.greased === true
+      })
+      console.log(greasedHogs)
+      this.setState({
+        hogs: greasedHogs,
+        greased: true
+      })
+    } else {
+      this.setState({
+        hogs: hogs,
+        greased: false
+      })
     }
   }
 
@@ -18,7 +45,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        {/* <Filter /> */}
+        <Filter toggleGreased={this.toggleGreased}/>
         <HogContainer hogs={this.state.hogs}/>
       </div>
       
